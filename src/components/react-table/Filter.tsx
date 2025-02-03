71ec53345d2117c549dca1ebba9f0913e4adc7ff
joaole <joaole64@gmail.com>
2025-02-03 15:57:07 -0300

    modified:   src/components/react-table/Filter.tsx

--- src/components/react-table/Filter.tsx
+++ src/components/react-table/Filter.tsx
@@ -1,5 +1,5 @@
 import { Column } from "@tanstack/react-table";
-import { DebouncedInput } from "./DebouncedInput";
+import { DebouncedInput } from "@/components/react-table/DebouncedInput";
 
 type Props<T> = {
   column: Column<T, unknown>;
@@ -16,16 +16,16 @@ export default function Filter<T>({ column }: Props<T>) {
     <>
       <datalist id={column.id + "list"}>
         {sortedUniqueValues.map((value, i) => (
-          <option key={`${i}-${column.id}`} value={value} />
+          <option value={value} key={`${i}-${column.id}`} />
         ))}
       </datalist>
       <DebouncedInput
         type="text"
-        value={columnFilterValue ?? ("" as string)}
+        value={(columnFilterValue ?? "") as string}
         onChange={(value) => column.setFilterValue(value)}
-        placeholder={`Search... ${
+        placeholder={`Search... (${
           [...column.getFacetedUniqueValues()].filter((arr) => arr[0]).length
-        }`}
+        })`}
         className="w-full border shadow rounded bg-card"
         list={column.id + "list"}
       />
