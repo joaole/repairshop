18f0c8a5d8b9e5eb4fe2d24402c06192bbadb737
joaole <joaole64@gmail.com>
2025-02-04 11:32:23 -0300

    modified:   package-lock.json 	modified:   package.json 	modified:   src/app/(rs)/customers/CustomerTable.tsx 	modified:   src/app/(rs)/customers/form/CustomerForm.tsx 	modified:   src/app/(rs)/customers/form/page.tsx 	modified:   src/lib/safe-action.ts

--- src/lib/safe-action.ts
+++ src/lib/safe-action.ts
@@ -1,6 +1,7 @@
 import { createSafeActionClient } from "next-safe-action";
 import { z } from "zod";
 import * as Sentry from "@sentry/nextjs";
+import type { NeonDbError } from "@neondatabase/serverless";
 
 export const actionClient = createSafeActionClient({
   defineMetadataSchema() {
@@ -11,13 +12,23 @@ export const actionClient = createSafeActionClient({
   handleServerError(e, utils) {
     // You can access these properties inside the `utils` object.
     const { clientInput, metadata } = utils;
+
+     if (e.constructor.name === "NeonDbError") {
+      const { code, detail } = e as NeonDbError;
+
+      if (code === "23505") {
+        return `Unique entry required. ${detail}`;
+      }
+    }
+
     Sentry.captureException(e, (scope) => {
         scope.clear()
         scope.setContext("serverError", {message: e.message })
         scope.setContext("metadata", {actionName: metadata?.actionName})
         scope.setContext("clientInput", { clientInput })
         return scope
-    })
+    });
+   
     if (e.constructor.name === "NeonDbError") {
       return "Database error: Your data did not save. Support will be notified.";
     }
