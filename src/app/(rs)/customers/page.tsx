cdf443a636a1e7ad84a5344580553fcc9a79e28d
joaole <joaole64@gmail.com>
2025-02-04 11:33:11 -0300

    modified:   src/app/(rs)/customers/CustomerSearch.tsx 	modified:   src/app/(rs)/customers/page.tsx

--- src/app/(rs)/customers/page.tsx
+++ src/app/(rs)/customers/page.tsx
@@ -1,7 +1,6 @@
 import CustomerSearch from "@/app/(rs)/customers/CustomerSearch";
 import { getCustomerSearchResults } from "@/lib/queries/getCustomerSearchResults";
 import CustomerTable from "@/app/(rs)/customers/CustomerTable";
-import * as Sentry from "@sentry/react";
 
 export const metadata = {
   title: "Customer Search",
@@ -16,13 +15,7 @@ export default async function Customers({
 
   if (!searchText) return <CustomerSearch />;
 
-  const span = Sentry.startInactiveSpan({
-    name: "getCustomerSearchResults-2",
-  });
-
   const results = await getCustomerSearchResults(searchText);
-
-  span.end();
 
   return (
     <>
